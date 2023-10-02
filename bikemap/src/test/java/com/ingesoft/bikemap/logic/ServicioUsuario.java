944f6fc69f594e15ea49cad82a73b041f66da935
Alejandro Barragán <alejo190404@gmail.com>
2023-10-02 16:53:58 +0000

    Avance Servicio Usuario

---
+++ bikemap/src/test/java/com/ingesoft/bikemap/logic/ServicioUsuario.java
@@ -0,0 +1,27 @@
+package com.ingesoft.bikemap.logic;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ingesoft.bikemap.dataAccess.RepositorioUsuario;
+
+@Service
+public class ServicioUsuario {
+    
+    @Autowired
+    private RepositorioUsuario repositorioUsuario;
+
+    public void CrearUsuario(
+        String nombreCompleto, //1
+        String nombreUsuario, //2
+        String contraseña, //4
+        String confirmacionContraseña, //6
+        String correoRecuperacion, //8
+        String confirmacionCorreoRecuperacion /*11*/ ){
+
+        repositorioUsuario.findById(null)
+
+
+    }
+
+}
