0935aa77ea75a238831ec187833acdce33e25a86
Alejandro Barragán <alejo190404@gmail.com>
2023-10-02 12:38:43 +0000

    Proyecto Spring Java creado

---
+++ bikemap/src/main/java/com/ingesoft/bikemap/BikemapApplication.java
@@ -0,0 +1,13 @@
+package com.ingesoft.bikemap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikemapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikemapApplication.class, args);
+	}
+
+}
---
+++ bikemap/src/test/java/com/ingesoft/bikemap/BikemapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ingesoft.bikemap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikemapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
