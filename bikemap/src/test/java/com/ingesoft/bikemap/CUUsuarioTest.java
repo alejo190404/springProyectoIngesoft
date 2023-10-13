9fac501deba299801b38c98b3555d6ae2e3340f4
Alejandro Barragán <alejo190404@gmail.com>
2023-10-13 05:20:25 +0000

    Pruebas CU Usuario finalizadas

--- bikemap/src/test/java/com/ingesoft/bikemap/CUUsuarioTest.java
+++ bikemap/src/test/java/com/ingesoft/bikemap/CUUsuarioTest.java
@@ -20,16 +20,16 @@ public class CUUsuarioTest {
     RepositorioUsuario repoUsuario;
 
     @BeforeEach
-    void resetear(){
+    void resetear() {
         repoUsuario.deleteAll();
     }
 
-    //CU Iniciar Sesión
+    // CU Iniciar Sesión
 
     @Test
-    void probarIniciarSesionExitoso() throws Exception{
+    void IniciarSesionExitoso() throws Exception {
         try {
-            //Arrange
+            // Arrange
 
             Usuario u = new Usuario();
             u.setNombreCompleto("prueba");
@@ -38,41 +38,41 @@ public class CUUsuarioTest {
             u.setCorreoRecuperacion("prueba@gmail.com");
             repoUsuario.save(u);
 
-            //Act
+            // Act
 
             servicio.IniciarSesion("prueba", "secret");
 
-            //Assert
-            
+            // Assert}
+
         } catch (Exception e) {
-            //Assert
-            fail();
+            // Assert
+            fail("El inicio de sesión no fue exitoso");
         }
     }
 
     @Test
-    void probarIniciarSesionUsuarioNoExiste() throws Exception{
+    void IniciarSesionUsuarioNoExiste() throws Exception {
         try {
-            //Arrange
+            // Arrange
 
-            //Act
+            // Act
 
             servicio.IniciarSesion("prueba", "prueba");
 
-            //Assert
+            // Assert
 
-            fail();
+            fail("Se encontró un usuario que no había sido registrado");
+
         } catch (Exception e) {
-            //Assert
-            
-            
+            // Assert
+
         }
     }
 
     @Test
-    void probarIniciarSesionContraseñaIncorrecta() throws Exception{
+    void IniciarSesionContraseñaIncorrecta() throws Exception {
         try {
-            //Arrange
+            // Arrange
 
             Usuario u = new Usuario();
             u.setNombreCompleto("prueba");
@@ -81,35 +81,52 @@ public class CUUsuarioTest {
             u.setCorreoRecuperacion("prueba@gmail.com");
             repoUsuario.save(u);
 
-            //Act
+            // Act
 
             servicio.IniciarSesion("prueba", "secretito");
 
-            //Assert
+            // Assert
 
-            fail();
+            fail("El sistema ingresó a un usuario con contraseña incorrecta");
         } catch (Exception e) {
-            //Assert
+            // Assert
 
-            
         }
     }
 
-    //CU Registrar Usuario
+    // CU Registrar Usuario
 
     @Test
-    void probarRegistro() throws Exception{
-        
+    void RegistrarUsuarioExitoso() throws Exception {
+
         try {
-            servicio.RegistrarUsuario("prueba",
-                                "prueba",
-                                "secret", //Todas las contraseñas de prueba serán secret
-                                "secret",
-                                "prueba@gmail.com",
-                                "prueba@gmail.com");
-        
+
+            // Arrange
+
+            // Act
+
+            servicio.RegistrarUsuario(
+                    "prueba",
+                    "prueba",
+                    "secret", // Todas las contraseñas de prueba serán secret
+                    "secret",
+                    "prueba@gmail.com",
+                    "prueba@gmail.com");
+
+            // Assert
+
+            Usuario u = repoUsuario.findByLogin("prueba");
+
+            assertEquals("prueba", u.getNombreCompleto(), "El sistema no guardó correctamente el nombre completo");
+            assertEquals("secret", u.getContraseña(), "El sistema no guardó correctamente la contraseña");
+            assertEquals("prueba@gmail.com", u.getCorreoRecuperacion(),
+                    "El sistema no guardó correctamente el nombre completo");
+            assertNotNull(u, "El objeto no se guardó correctamente");
+
         } catch (Exception e) {
-            fail();
+            // Assert
+
+            fail("El sistema no guardó al usuario");
         }
 
     }
@@ -117,126 +134,188 @@ public class CUUsuarioTest {
     @Test
     void probarRegistroNombreExistente() throws Exception {
         try {
-            servicio.RegistrarUsuario("prueba",
-                                    "prueba",
-                                    "secret",
-                                    "secret",
-                                    "prueba2@gmail.com",
-                                    "prueba2@gmail.com");
-            fail();
+            // Arrange
+
+            Usuario u = new Usuario();
+
+            u.setNombreCompleto("Jorge Vargas");
+            u.setLogin("jorgeVargas");
+            u.setContraseña("secret");
+            u.setCorreoRecuperacion("info@paisasclub.co");
+
+            // Act
+
+            servicio.RegistrarUsuario(
+                    "prueba",
+                    "jorgeVargas",
+                    "secret",
+                    "secret",
+                    "prueba2@gmail.com",
+                    "prueba2@gmail.com");
+
+            // Assert
+
+            fail("El sistema registró un usario con nombre de usuario existente");
+
         } catch (Exception e) {
-            
+            // Assert
+
         }
     }
 
     @Test
-    void probarRegistroContraseniaCorta() throws Exception{
+    void probarRegistroContraseniaCorta() throws Exception {
         try {
-            servicio.RegistrarUsuario("prueba3",
-                                    "prueba3",
-                                    "sret",
-                                    "sret",
-                                    "prueba3@gmail.com",
-                                    "prueba3@gmail.com");
-            fail();
+            // Arrange
+
+            // Act
+
+            servicio.RegistrarUsuario(
+                    "prueba3",
+                    "prueba3",
+                    "sret",
+                    "sret",
+                    "prueba3@gmail.com",
+                    "prueba3@gmail.com");
+
+            // Assert
+
+            fail("El sistema registró un usuario con contraseña inválida (menos de 6 caracteres)");
+
         } catch (Exception e) {
-            // TODO: handle exception
+            // Assert
+
         }
     }
 
     @Test
-    void probarRegistroContraseniaDiferente() throws Exception{
+    void probarRegistroContraseniaDiferente() throws Exception {
         try {
-            servicio.RegistrarUsuario("prueba4",
-                                    "prueba4",
-                                    "secretito",
-                                    "sret",
-                                    "prueba4@gmail.com",
-                                    "prueba4@gmail.com");
-            fail();
+            // Arrange
+
+            // Act
+
+            servicio.RegistrarUsuario(
+                    "prueba4",
+                    "prueba4",
+                    "secretito",
+                    "sret",
+                    "prueba4@gmail.com",
+                    "prueba4@gmail.com");
+
+            // Assert
+
+            fail("El sistema registró un usaurio con contraseña de confirmacion que no coincide");
+
         } catch (Exception e) {
-            // TODO: handle exception
+            // Assert
         }
     }
 
     @Test
-    void probarRegistroCorreoSinSintaxisUno() throws Exception{
+    void probarRegistroCorreoSinSintaxisUno() throws Exception {
         try {
-            servicio.RegistrarUsuario("prueba5",
-                                "prueba5",
-                                "secret", //Todas las contraseñas de prueba serán secret
-                                "secret",
-                                "pruebagmail.com",
-                                "pruebagmail.com");
-            fail();
-        } catch (Exception e) {
-            // TODO: handle exception
-        }
-    }
 
-    /*@Test
-    void probarRegistroCorreoSinSintaxisDos() throws Exception{
-        try {
-            servicio.RegistrarUsuario("prueba5",
-                                "prueba5",
-                                "secret", //Todas las contraseñas de prueba serán secret
-                                "secret",
-                                "prueba@gmail.",
-                                "prueba@gmail.");
-            fail();
+            // Arrange
+
+            // Act
+
+            servicio.RegistrarUsuario(
+                    "prueba5",
+                    "prueba5",
+                    "secret",
+                    "secret",
+                    "pruebagmail.com",
+                    "pruebagmail.com");
+
+            // Assert
+
+            fail("El sistema registra un usuario con correo inválido");
+
         } catch (Exception e) {
-            // TODO: handle exception
-        }
-    }
-*/
-    @Test
-    void probarRegistroCorreoExistente() throws Exception{
-        try {
-            servicio.RegistrarUsuario("prueba7",
-                                "prueba7",
-                                "secret", //Todas las contraseñas de prueba serán secret
-                                "secret",
-                                "prueba@gmail.com",
-                                "prueba@gmail.com");
-            fail();
-        } catch (Exception e) {
-            // TODO: handle exception
+            // Assert
         }
     }
 
     @Test
-    void probarRegsistroCorreosDiferentes() throws Exception{
+    void probarRegistroCorreoSinSintaxisDos() throws Exception {
+
         try {
-            servicio.RegistrarUsuario("prueba8",
-                                "prueba8",
-                                "secret", //Todas las contraseñas de prueba serán secret
-                                "secret",
-                                "prueba8@gmail.com",
-                                "prueba88@gmail.com");
-            fail();
+
+            // Arrange
+
+            // Act
+
+            servicio.RegistrarUsuario(
+                    "prueba5",
+                    "prueba5",
+                    "secret", // Todas las contraseñas de prueba serán secret
+                    "secret",
+                    "prueba@gmail.",
+                    "prueba@gmail.");
+
+            // Assert
+
+            fail("El sistema registra un usuario con correo inválido");
+
         } catch (Exception e) {
-            // TODO: handle exception
+            // Assert
         }
     }
 
     @Test
-    void probarRegistroExistente() throws Exception{
+    void probarRegistroCorreoExistente() throws Exception {
         try {
-            servicio.RegistrarUsuario("prueba9",
-                                "prueba9",
-                                "secret", //Todas las contraseñas de prueba serán secret
-                                "secret",
-                                "prueba9@gmail.com",
-                                "prueba9@gmail.com");
+            // Arrange
 
-            Usuario u = repoUsuario.findByLogin("prueba9");
-            
-            if (u == null){
-                fail();
-            }
+            servicio.RegistrarUsuario(
+                    "prueba7",
+                    "prueba7",
+                    "secret", // Todas las contraseñas de prueba serán secret
+                    "secret",
+                    "prueba@gmail.com",
+                    "prueba@gmail.com");
+
+            // Act
+
+            servicio.RegistrarUsuario(
+                    "Jorgito",
+                    "giorgio",
+                    "secret", // Todas las contraseñas de prueba serán secret
+                    "secret",
+                    "prueba@gmail.com",
+                    "prueba@gmail.com");
+
+            // Assert
+
+            fail("El sistema registró un usuario con correo de recuperacion existente");
 
         } catch (Exception e) {
-            fail();
+            // Assert
+        }
+    }
+
+    @Test
+    void probarRegsistroCorreosDiferentes() throws Exception {
+        try {
+            // Arrange
+
+            // Act
+
+            servicio.RegistrarUsuario(
+                    "prueba8",
+                    "prueba8",
+                    "secret", // Todas las contraseñas de prueba serán secret
+                    "secret",
+                    "prueba8@gmail.com",
+                    "prueba88@gmail.com");
+
+            // Assert
+
+            fail("El sistema registra un usuario con confirmacion de correo diferente");
+
+        } catch (Exception e) {
+            // Assert
         }
     }
 }
