36f9ddde6f0ec8e5962c0f82c2965dfad31f60d1
Alejandro Barragán <alejo190404@gmail.com>
2023-10-17 14:01:52 +0000

    Avance pruebas CU Ruta

--- bikemap/src/test/java/com/ingesoft/bikemap/CURutaTest.java
+++ bikemap/src/test/java/com/ingesoft/bikemap/CURutaTest.java
@@ -1,5 +1,9 @@
 package com.ingesoft.bikemap;
 
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +12,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import com.ingesoft.bikemap.dataAccess.RepositorioCalificacion_Ruta;
 import com.ingesoft.bikemap.dataAccess.RepositorioRuta;
 import com.ingesoft.bikemap.dataAccess.RepositorioUsuario;
+import com.ingesoft.bikemap.dominio.Ruta;
 import com.ingesoft.bikemap.logic.ServicioRuta;
+
+import javafx.util.Pair;
 
 @SpringBootTest
 public class CURutaTest {
@@ -36,11 +43,30 @@ public class CURutaTest {
         try {
             //Arrange
 
+            Usuario u = new Usuario();
 
+            u.set
+            
+            repositorioUsuario.save(u);
+
+            Date fecha = Date.valueOf("2023-10-17");
+            Pair p1 = new Pair("4", "6");
+            Pair p2 = new Pair("5", "7");
+            Pair p3 = new Pair("6", "8");
+            List<Pair<String, String>> puntos = new ArrayList<>();
+            puntos.add(0, p1);
+            puntos.add(1, p2);
+            puntos.add(2, p3);
 
             //Act
 
-
+            servicio.CrearRuta(
+                "Unviersidades",
+                "Recorrido por las principales universidades de Bogotá",
+                puntos,
+                fecha,
+                "null"
+            );
 
             //Assert
 
